d541af2845d331e7d99b6260ce5912953f093b69
Jose JS. Suarez <jsuarez@branar.com>
2022-05-11 14:57:20 -0400

    'navbar'

--- pages/index.js
+++ pages/index.js
@@ -1,74 +1,13 @@
-import { useState } from 'react'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
-  const [isActive, setIsActive] = useState(false)
   return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
-          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
-        </a>
-
-        <a role="button" 
-          className={`navbar-burger ${isActive ? 'is-active' : null}`} 
-          aria-label="menu" aria-expanded="false" 
-          data-target="navbarBasicExample"
-          onClick={e => setIsActive(!isActive)}>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-        </a>
-      </div>
-
-      <div id="navbarBasicExample" className={`navbar-menu ${isActive ? 'is-active' : null}`}>
-        <div className="navbar-start">
-          <a className="navbar-item">
-            Home
-          </a>
-
-          <a className="navbar-item">
-            Documentation
-          </a>
-
-          <div className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link">
-              More
-            </a>
-
-            <div className="navbar-dropdown">
-              <a className="navbar-item">
-                About
-              </a>
-              <a className="navbar-item">
-                Jobs
-              </a>
-              <a className="navbar-item">
-                Contact
-              </a>
-              <hr className="navbar-divider" />
-              <a className="navbar-item">
-                Report an issue
-              </a>
-            </div>
-          </div>
-        </div>
-
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons">
-              <a className="button is-primary">
-                <strong>Sign up</strong>
-              </a>
-              <a className="button is-light">
-                Log in
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>  
+    <>
+      <Head> 
+        <title>Home</title>
+      </Head>
+      <Navbar></Navbar>
+    </>
   )
 }
